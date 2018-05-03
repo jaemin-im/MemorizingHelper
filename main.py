6d41f7698bda9ffa9e21444db6f0a4ed1dd33083
Xsolute Lim <Xsolute01@gmail.com>
2018-05-03 20:36:53 +0900

    add init printing

---
+++ main.py
@@ -0,0 +1,7 @@
+import mhinfo
+
+print('Term Memorizing Program with Python: v{}'.format(mhinfo.version))
+print('1. Add Term')
+print('2. Modify Term')
+print('3. Practice Term')
+print('4. Test Term')
