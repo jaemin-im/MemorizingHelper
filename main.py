7b6384f253a2139967b770ae24f05b6ab025a032
Xsolute Lim <Xsolute01@gmail.com>
2018-05-04 13:56:49 +0900

    add selection, end

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ print('1. Add Term')
 print('2. Modify Term')
 print('3. Practice Term')
 print('4. Test Term')
+print('0. Exit')
+
+select = int(input("Input the number:\n"))
+
+if select == 1:
+    pass # Add Term
+elif select == 2:
+    pass # Modify Term
+elif select == 3:
+    pass # Practice Term
+elif select == 4:
+    pass # Test Term
+elif select == 0:
+    print('Interrupting Program. Bye!')
+    exit()
