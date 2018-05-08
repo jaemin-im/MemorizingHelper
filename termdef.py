79c26ce88aa234a464740a09c6db986310374617
Xsolute Lim <Xsolute01@gmail.com>
2018-05-08 23:13:00 +0900

    mod termdef, bug catch

--- termdef.py
+++ termdef.py
@@ -8,10 +8,9 @@ def addterm():
     print("You should input the term and mean.")
     term = str(input("Input the term: "))
     mean = str(input("Input the mean: "))
-    date = (datetime.datetime.now()).strftime('%Y%m%d')
+    date = str((datetime.datetime.now()).strftime('%Y%m%d'))
     try:
-        cursor.execute("INSERT INTO term VALUES({}, {}, {}, {}, {})".format(date, term, mean, 0, 0))
-        # TODO: modify db inserting system: error occurs in multi-string input.
+        cursor.execute("INSERT INTO term VALUES(\"{}\", \"{}\", \"{}\", {}, {})".format(date, term, mean, 0, 0))
     except:
         print("Error has occured while adding term to database.")
         return
